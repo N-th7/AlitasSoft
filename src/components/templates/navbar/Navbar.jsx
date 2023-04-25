ddb22f12e31870279815d94ca35a39819fcf2fec
N-th7 <64654131+N-th7@users.noreply.github.com>
2023-04-25 18:03:19 -0400

    vista menu principal implementada

---
+++ src/components/templates/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './navbar.css';
+import 'w3-css/w3.css';
+
+const Navbar = () => {
+    return(
+
+        <div className="w3-bar-block navbarContent">
+            <button className='login' id='login'>Iniciar sesión</button>
+        </div>
+    )
+
+}
+export default Navbar
